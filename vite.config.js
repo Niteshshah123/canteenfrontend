d23648c55d77c35336dd26f0c8c809e65edd1ea3
Niteshshah123 <niteshshah011@gmail.com>
2025-11-30 02:26:19 +0530

    update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -5,7 +5,7 @@ import path from 'path'
 
 // https://vite.dev/config/
 export default defineConfig(({ mode }) => ({
-  base: mode === "production" ? "/amritacanteen/" : "/",
+  base: "/",
   plugins: [react(),tailwindcss()],
   resolve: {
     alias: {
